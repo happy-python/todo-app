c2cecfa723c0e070cdc90323aa76045b88929d08
zhangyongjie <1091354206@qq.com>
2017-05-12 16:02:42 +0800

    格式化代码

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -8,47 +8,47 @@
         <p class="tasks">Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
     
         <!--we are now passing the data to the todo component to render the todo list-->
-
+    
         <Todo v-for="(todo, index) in todos" :todo="todo" :key="index" @delete-todo="deleteTodo" @complete-todo="completeTodo"></Todo>
     </div>
 </template>
 
 <script>
-    import Todo from './Todo'
+import Todo from './Todo'
 
-    export default {
-        props: ['todos'],
-        components: {
-            Todo
-        },
-        methods:{
-            deleteTodo(todo){
-                swal({
-                    title: "Are you sure?",
-                    text: "This To-Do will be permanently deleted!",
-                    type: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#DD6B55",
-                    confirmButtonText: "Yes, delete it!",
-                    closeOnConfirm: false
-                    },
-                    () => {
-                        const todoIndex = this.todos.indexOf(todo)
-                        this.todos.splice(todoIndex, 1)
-                        swal("Deleted!", "Your To-Do has been deleted.", "success")
-                    });
+export default {
+    props: ['todos'],
+    components: {
+        Todo
+    },
+    methods: {
+        deleteTodo(todo) {
+            swal({
+                title: "Are you sure?",
+                text: "This To-Do will be permanently deleted!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, delete it!",
+                closeOnConfirm: false
             },
-            completeTodo(todo) {
-                const todoIndex = this.todos.indexOf(todo)
-                this.todos[todoIndex].done = true
-                swal("Success!", "To-Do completed!", "success")
-            }
+                () => {
+                    const todoIndex = this.todos.indexOf(todo)
+                    this.todos.splice(todoIndex, 1)
+                    swal("Deleted!", "Your To-Do has been deleted.", "success")
+                });
+        },
+        completeTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo)
+            this.todos[todoIndex].done = true
+            swal("Success!", "To-Do completed!", "success")
         }
     }
+}
 </script>
 
 <style>
-    .tasks {
-        text-align: center;
-    }
+.tasks {
+    text-align: center;
+}
 </style>
